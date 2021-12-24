4b9f0b5d3581b896ff1099ab320cc9f0c58df220
denims <denimallel@gmail.com>
2021-12-24 18:38:56 +0530

    Added layout using row and column widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(BusinessCardApp());
+}
+
+class BusinessCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Container(
+                color: Colors.teal,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.lightGreen,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
