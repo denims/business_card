96ba367cc0ce5e546539dfb8765a1c8cb9fadbc4
denims <denimallel@gmail.com>
2022-01-28 15:45:19 +0530

    Completed Business card app

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BusinessCardApp());
+  runApp(const BusinessCardApp());
 }
 
 class BusinessCardApp extends StatelessWidget {
+  const BusinessCardApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                width: 100,
+              const CircleAvatar(
+                radius: 90,
+                backgroundImage: AssetImage("images/Deni.jpeg"),
               ),
-              Container(
-                color: Colors.teal,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.yellow,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.lightGreen,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ],
+              const Text(
+                "Deni Mathew Simon",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "CEO & Lead Software Developer",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "SourceSansPro",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "RainyDay Labs",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "SourceSansPro",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
               ),
-              Container(
-                color: Colors.blue,
-                width: 100,
+              Card(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+91-9xxxxxxxxx",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "SourceSansPro",
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "d*****@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "SourceSansPro",
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
